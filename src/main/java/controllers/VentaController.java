7599b08595e1d84c856636cdd7af1a70bf9e7f41
Vindelj13 <brandonjoelmc@gmail.com>
2024-07-17 11:18:49 -0600

    github link

--- src/main/java/controllers/VentaController.java
+++ src/main/java/controllers/VentaController.java
@@ -1,9 +1,12 @@
 package controllers;
 
 import dao.VentaDao;
+import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
 import models.Venta;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,8 +26,13 @@ public class VentaController {
     }
 
     @PostMapping("api/ventas")
-    public void insertVenta(@RequestBody Venta venta) {
-        ventaDao.insertVenta(venta);
+    public ResponseEntity<String> insertVenta(@RequestBody Venta venta) {
+        try {
+            ventaDao.insertVenta(venta);
+            return ResponseEntity.ok("La venta se registró con éxito");
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No se pudo procesar la solicitud: Producto no encontrado");
+        }
     }
 
     @GetMapping("api/ventas/{id}")
