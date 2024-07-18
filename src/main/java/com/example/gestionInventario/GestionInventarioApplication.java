d8cc04507075bd466f58d043602c3cbdb2aba46e
Vindelj13 <brandonjoelmc@gmail.com>
2024-07-18 00:05:59 -0600

    final

--- src/main/java/com/example/gestionInventario/GestionInventarioApplication.java
+++ src/main/java/com/example/gestionInventario/GestionInventarioApplication.java
@@ -1,6 +1,5 @@
 package com.example.gestionInventario;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -9,18 +8,10 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = {"controllers", "dao"})
 @EntityScan("models")
-public class GestionInventarioApplication implements CommandLineRunner {
+public class GestionInventarioApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GestionInventarioApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.println("MYSQL_DATABASE: " + System.getenv("MYSQL_DATABASE"));
-        System.out.println("RAILWAY_TCP_PROXY_DOMAIN: " + System.getenv("RAILWAY_TCP_PROXY_DOMAIN"));
-        System.out.println("MYSQL_ROOT_PASSWORD: " + System.getenv("MYSQL_ROOT_PASSWORD"));
-        System.out.println("RAILWAY_TCP_PROXY_PORT: " + System.getenv("RAILWAY_TCP_PROXY_PORT"));
     }
 
 }
